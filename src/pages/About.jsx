2811e9d5c3352d0a0e6420ecf1b99c2b25fde581
IliaKachnov <123602127+IliaKachnov@users.noreply.github.com>
2024-01-14 08:38:14 +0100

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,7 +6,7 @@ import Advantages from "../components/advantages/Advantages"
 import CarCard from "../components/carCard/CarCard";
 import Follow from "../components/follow/Follow";
 import FormRent from "../components/form/FormRent"
-import FormChauf from "../components/formChauf/formChauf";
+import FormChauf from "../components/formChauf/FormChauf";
 import Reviews from "../components/reviews/Reviews";
 
 import '../styles/about.css';
